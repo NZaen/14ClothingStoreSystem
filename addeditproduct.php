de552dc9c0e17a6596ee8aff9e13dd1b0a4473ca
MoldCig <77414919+MoldCig@users.noreply.github.com>
2023-01-17 00:52:15 +0800

    Add files via upload

--- addeditproduct.php
+++ addeditproduct.php
@@ -4,7 +4,6 @@ if(isset($_POST['submit']))
     $Pname = $_POST['Pname'];
     $price = $_POST['price'];
     $category = $_POST['category'];
-    $amount = $_POST['amount'];
     //$files = $_POST['file'];
 
     $host = "localhost";
@@ -31,7 +30,7 @@ if(isset($_POST['submit']))
     }
 
     // using sql to create a data entry query
-    $sql = "INSERT INTO product (Pid, Pname, price, amount, category,fileExt) VALUES ('0', '$Pname', '$price', '$amount', '$category', '$fileActualExt')";
+    $sql = "INSERT INTO product (Pid, Pname, price, category,fileExt) VALUES ('0', '$Pname', '$price', '$category', '$fileActualExt')";
 
     mysqli_query($con, $sql);
 
